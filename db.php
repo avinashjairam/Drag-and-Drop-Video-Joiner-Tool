7f5b33a4ea78721f8a85fdc2667fdec83ce89119
Avinash <avinash.jairam@gmail.com>
2016-03-06 23:33:29 -0500

    starting working on CRUD functions

--- db.php
+++ db.php
@@ -1,10 +1,35 @@
 <?php
 
+//Database Class
+//This class has functions which perform CRUD operations
 class DB{
 
 	//The database connection 
 	protected static $connection;
 
+	public function connect(){
+
+	}
+
+	public function disconnect(){
+
+	}
+
+	public function select(){
+
+	}
+
+	public function insert(){
+
+	}
+
+	public function delete(){
+
+	}
+
+	public function update(){
+
+	}
 
 
 
