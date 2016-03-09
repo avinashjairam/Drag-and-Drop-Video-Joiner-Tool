34b6bb4d406522dfcd7f75ae8badf510e1ffee2d
Avinash <avinash.jairam@gmail.com>
2016-03-08 23:59:26 -0500

    Added db connect method, constructor, and required config file

--- db.php
+++ db.php
@@ -1,11 +1,27 @@
 <?php
 
+require_once("config.php");
 //Database Class
 //This class has functions which perform CRUD operations
 class DB{
 
 	//The database connection 
-	protected static $connection;
+	public $connection;
+
+
+	public function __construct(){
+		$this->openDBConnection();
+	}
+
+	public function openDBConnection(){
+		$this->connection = mysqli_connect(DB_HOST, DB_USER,DB_PASS,DB_NAME);
+
+		if(mysqli_connect_error()){
+			die("Database connection failed badly" . mysqli_error());
+		}
+
+	}
+
 
 	public function connect(){
 
@@ -36,7 +52,8 @@ class DB{
 }
 
 
-
+$database = new Database();
+$database->openDBConnection();
 
 
 ?>
