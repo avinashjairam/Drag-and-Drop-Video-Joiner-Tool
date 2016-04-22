b4c44298844c4f22face9e997fbbd78d6e421425
Avinash <avinash.jairam@gmail.com>
2016-04-22 19:56:19 -0400

    Program successfully writes name of tracks to FilesToMerge.txt

--- joinVideos.php
+++ joinVideos.php
@@ -2,29 +2,18 @@
 
  require_once ('./includes/session.php');
  $session = new Session;
-	 // echo session_id();
 
 if($_POST['str']){
-	// parse_str($_POST['str'], $tracks);
-
-	// print_r($tracks);
-	// for($index=0; $index <count($tracks); $index++){
-	// 	echo $tracks[$index]."<br>";
-	// }
-	// $tracksUploaded = array();
 
 	 $tracksUploaded = json_decode($_POST['str'], true);
 	 $targetFolder= $session->getSessionID();
 
-	 //echo session_id();
+	 $myfile = fopen("./$targetFolder/filesToMerge.txt", "w") or die("Unable to open file!");
 
-	 for($index=0; $index <count($tracksUploaded); $index++){
-		echo $tracksUploaded[$index]."<br>";
+	  for($index=0; $index <count($tracksUploaded); $index++){
+	     $tracksUploaded[$index]."\n";
+	     fwrite($myfile, $tracksUploaded[$index]);
 	}
-
-	$myfile = fopen("./$targetFolder/newfile.txt", "w") or die("Unable to open file!");
-
-	//echo $tracksUploaded[0];
 
 	print_r($_POST['str']);
 
