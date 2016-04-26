61283da4a958f3a1a914f02931a0104fd0a80cf5
Avinash <avinash.jairam@gmail.com>
2016-04-26 00:42:41 -0400

    Added stick footer to homepage

--- frontEnd.php
+++ frontEnd.php
@@ -263,6 +263,12 @@ window.onload=function(){
 
 </div>
 
+<footer class="footer">
+      <div class="container">
+        <p class="text-muted">Place sticky footer content here.</p>
+      </div>
+</footer>
+
 
 
 <script>
