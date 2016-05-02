b277f43f39d26d44520e9cd6d6dae8fc699a13d1
Avinash <avinash.jairam@gmail.com>
2016-05-01 23:54:44 -0400

    Modified joinVideos.php to edit the script mergeUploadedFiles.sh. Hence, a user can now merge multiple videos several times during a single session

--- joinVideos.php
+++ joinVideos.php
@@ -5,6 +5,8 @@
  $session = new Session;
  //$user    = new User;
 
+ $increment = 1;
+$_SESSION['mergeCount'] += $increment;
  //$user->setSessionId($session->getSessionId());
 
 if($_POST['str']){
@@ -13,9 +15,23 @@ if($_POST['str']){
 	 $targetFolder= $session->getSessionID();
 
 	 $myfile = fopen("./$targetFolder/filesToMerge.txt", "w") or die("Unable to open file!");
+	 $myfile2 = fopen("./mergeUploadedFiles.sh", "w") or die("Unable to open file!");
+
+	 $mergedFileName="output".$_SESSION['mergeCount'].".mp4";
+
+	// echo $mergedFileName."<br>";
+
+	// fwrite($myfile2," ".$mergedFileName);
+	 file_put_contents($my, '');
+
+	 $ffmpegCommand = "ffmpeg -f concat -i filesToMerge.txt -c copy $mergedFileName";
+	 //fwrite($myfile2,$ffmpegCommand);
+	// echo $ffmpegCommand."<br>";
 
 	 $mergeCommand = "cd $targetFolder && bash ../mergeUploadedFiles.sh";
+	 //$mergeCommand = "cd $targetFolder && $ffmpegCommand";
 
+	 fwrite($myfile2, $ffmpegCommand);
 
 	  for($index=0; $index <count($tracksUploaded); $index++){
 	     $tracksUploaded[$index] = 'file ' . "'". rtrim($tracksUploaded[$index])."'"."\n";
@@ -23,12 +39,13 @@ if($_POST['str']){
 	}
 
 	  fclose($myfile);
+	  fclose($myfile2);
 	//  echo $user->clearUploadedTracks();
 
 	exec($mergeCommand,$output,$return);
 
 	if($return==0){
-		print "./$targetFolder/output.mp4";
+		print "./$targetFolder/$mergedFileName";
 		// $user->clearUploadedTracks();
 	}
 
