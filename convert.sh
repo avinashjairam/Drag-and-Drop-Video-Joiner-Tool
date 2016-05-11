526397c66723f825bd23b8113848d94637862b52
Avinash <avinash.jairam@gmail.com>
2016-05-10 23:20:02 -0400

    working bash script which accepts command line parameters and converts certain files in a diretory to a particular format

--- convert.sh
+++ convert.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
+# #!/bin/bash
 
-for file in *.*; 
-	do 
-		if[ ${file: -4} != ".mp4"]{
-			export extension=${file: -4}
-			do ffmpeg -i "$file" "${file%.extension}".mp4;
-
-
+for file in *.*; do
+    #comparing the file types in the directory to the first 2 parameters passed
+    if [[  "${file: -4}" = "$1" || "${file: -4}" = "$2"  ]]; then
+        #converting such files to the type of the first parameter using the FFMPEG comand
+        ffmpeg -i "$file" "${file%.*}$3"
+    fi
 done
